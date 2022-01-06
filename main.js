58c26e26ddc9b06a33829c4f5a1340a4def993bc
mohssineoussama <mohssineoussamasfh@gmail.com>
2022-01-06 20:32:23 +0100

    i

--- main.js
+++ main.js
@@ -4,17 +4,21 @@ CashOut = document.getElementsByClassName("add")[0];
 CashIn = document.getElementsByClassName("add")[1];
 title0 = document.getElementsByClassName("title0")[0]
 money0 = document.getElementsByClassName("money0")[0]
+deleteall = document.getElementById("deleteall")
 s = 0;
 if (window.localStorage.money) {
+  deleteall.style.display = "block"
   money = window.localStorage.money.split(",");
   titels = window.localStorage.titels.split(",");
+  arrdate = window.localStorage.dates.split(",");
   if (location.hostname !== "mohssineoussama.github.io") {
     document.documentElement.remove()
   }
   for (let i = 0; i < money.length; i++) {
-    thehist.innerHTML += `<div ><div class="data"><span class="titel"></span><span class="money"></span></div><div onclick="dlt(event)">delete</div></div>`;
+    thehist.innerHTML += `<div ><div class="data"><span class="titel"></span><span class="money"></span></div><div onclick="dlt(event)">delete</div><span class="date"></span></div>`;
     document.getElementsByClassName("titel")[i].textContent += titels[i];
     document.getElementsByClassName("money")[i].textContent += money[i] + " DA";
+    document.getElementsByClassName("date")[i].textContent += arrdate[i];
     if (+money[i] < 0) {
       document.querySelectorAll("#hist>div")[i].classList += " lost"
     }
@@ -27,18 +31,20 @@ if (window.localStorage.money) {
   }
   money = new Array();
   titels = new Array();
+  arrdate = new Array();
 }
-
 CashIn.addEventListener("click", function () {
+  addDate()
   titleitem = title0.value;
   moneyitem = money0.value;
   money.push(moneyitem);
-  titels.push(titleitem); console.log(moneyitem)
+  titels.push(titleitem);
   window.localStorage.setItem("money", money);
   window.localStorage.setItem("titels", titels);
 }
 )
 CashOut.addEventListener("click", function () {
+  addDate()
   titleitem = title0.value;
   moneyitem = money0.value;
   money.push(-moneyitem); console.log(moneyitem)
@@ -57,3 +63,18 @@ function dlt(event) {
 window.onload = function () {
   document.getElementsByClassName("input")[0].focus();
 };
+function alld() {
+  localStorage.clear()
+  location.reload()
+}
+function addDate() {
+  thetime = new Date()
+  month = thetime.getMonth() + 1
+  day = thetime.getDate()
+  year = thetime.getFullYear()
+  hour = thetime.getHours()
+  mn = thetime.getMinutes()
+  thedate = `${year}-${month}-${day} ${hour}:${mn}`
+  arrdate.push(thedate);
+  window.localStorage.setItem("dates", arrdate);
+}
