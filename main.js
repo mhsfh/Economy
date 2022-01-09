b61abc167cc7cc7891b68540e6d3b8b35d84cac0
mohssineoussama <mohssineoussamasfh@gmail.com>
2022-01-09 14:40:23 +0100

    i

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ money0 = document.getElementsByClassName("money0")[0]
 deleteall = document.getElementById("deleteall")
 s = 0;
 if (window.localStorage.money) {
-  deleteall.style.display = "block"
   money = window.localStorage.money.split(",");
   titels = window.localStorage.titels.split(",");
   arrdate = window.localStorage.dates.split(",");
   if (location.hostname !== "mohssineoussama.github.io") {
     document.documentElement.remove()
   }
+  thehist.innerHTML = `<span onclick="alld()" id="deleteall">Reset</span>`
   for (let i = 0; i < money.length; i++) {
     thehist.innerHTML += `<div ><div class="data"><span class="titel"></span><span class="money"></span></div><div onclick="dlt(event)">delete</div><span class="date"></span></div>`;
     document.getElementsByClassName("titel")[i].textContent += titels[i];
@@ -24,49 +24,24 @@ if (window.localStorage.money) {
     } s += +money[i]
   } total.textContent = s + " DA"
 } else {
+  deleteall.style.display = "none"
   if (location.hostname !== "mohssineoussama.github.io") { document.documentElement.remove() }
   money = new Array();
   titels = new Array();
   arrdate = new Array();
 }
-CashIn.addEventListener("click", function () {
-  titleitem = title0.value;
-  moneyitem = money0.value;
-  chkit = titleitem.match(/^\s*$/gi)
-  chkit0 = moneyitem.match(/^\s*$/gi)
-  chkit1 = moneyitem.match(/^[0-9]+$/gi)
-  if (chkit == null && chkit0 == null && chkit1 != null) {
-    addDate()
-    money.push(moneyitem);
-    titels.push(titleitem);
-    window.localStorage.setItem("money", money);
-    window.localStorage.setItem("titels", titels);
-  }
-})
-CashOut.addEventListener("click", function () {
-  titleitem = title0.value;
-  moneyitem = money0.value;
-  chkit = titleitem.match(/^\s*$/gi)
-  chkit0 = moneyitem.match(/^\s*$/gi)
-  if (chkit == null && chkit0 == null) {
-    addDate()
-    money.push(-moneyitem); console.log(moneyitem)
-    titels.push(titleitem);
-    window.localStorage.setItem("money", money);
-    window.localStorage.setItem("titels", titels);
-  }
-})
 function dlt(event) {
   target = event.currentTarget.parentElement;
   gparent = target.parentElement;
-  indexT = Array.prototype.indexOf.call(gparent.children, target) - 1
-  money.splice(indexT, 1);
-  titels.splice(indexT, 1);
-  arrdate.splice(indexT, 1);
+  T = Array.prototype.indexOf.call(gparent.children, target) - 1
+  money.splice(T, 1);
+  titels.splice(T, 1);
+  arrdate.splice(T, 1);
   window.localStorage.setItem("money", money);
   window.localStorage.setItem("dates", arrdate);
   window.localStorage.setItem("titels", titels);
   event.currentTarget.parentElement.remove()
+  location.reload()
 }
 window.onload = function () {
   document.getElementsByClassName("input")[0].focus();
@@ -77,12 +52,19 @@ function alld() {
 }
 function addDate() {
   thetime = new Date()
-  month = thetime.getMonth() + 1
-  day = thetime.getDate()
-  year = thetime.getFullYear()
-  hour = thetime.getHours()
-  mn = thetime.getMinutes()
-  thedate = `${year}-${month}-${day} ${hour}:${mn}`
+  month = ((thetime.getMonth() + 1) * 0.01).toFixed(2).replace("0.", '')
+  day = (thetime.getDate() * 0.01).toFixed(2).replace("0.", '')
+  thedate = `${thetime.getFullYear()}-${month}-${day} ${thetime.getHours()}:${thetime.getMinutes()}`
   arrdate.push(thedate);
   window.localStorage.setItem("dates", arrdate);
+}
+function cash(t) {
+  t === "out" ? money0.value = -money0.value : null;
+  if ((/^[1-9][0-9]*$/).test(money0.value) && !(/^\s*$/).test(title0.value)) {
+    money.push(money0.value);
+    titels.push(title0.value);
+    window.localStorage.setItem("money", money);
+    window.localStorage.setItem("titels", titels);
+    addDate()
+  }
 }
