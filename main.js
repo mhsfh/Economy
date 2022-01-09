9a5944f6e14a52833965c31bc3af68783833cd26
mohssineoussama <mohssineoussamasfh@gmail.com>
2022-01-09 17:51:56 +0100

    r

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-if (localStorage.vrf !== 'true') {
-  localStorage.money = JSON.stringify(localStorage.money.split(","))
-  localStorage.titels = JSON.stringify(localStorage.titels.split(","))
-  localStorage.dates = JSON.stringify(localStorage.dates.split(","))
-}
-localStorage.setItem("vrf", true)
-
 thehist = document.getElementById("hist");
 total = document.getElementById("total");
 CashOut = document.getElementsByClassName("add")[0];
@@ -14,11 +7,10 @@ money0 = document.getElementsByClassName("money0")[0]
 deleteall = document.getElementById("deleteall")
 s = 0;
 if (localStorage.money) {
-  // localStorage.money = JSON.stringify(localStorage.money.split(","))
   money = JSON.parse(localStorage.money);
   titels = JSON.parse(localStorage.titels);
   arrdate = JSON.parse(localStorage.dates);
-  // if (location.hostname !=="mohssineoussama.github.io"){document.documentElement.remove()}
+  if (location.hostname !== "mohssineoussama.github.io") { document.documentElement.remove() }
   thehist.innerHTML = `<span onclick="alld()" id="deleteall">Reset</span>`
   for (let i = 0; i < money.length; i++) {
     thehist.innerHTML += `<div ><div class="data"><span class="titel"></span><span class="money"></span></div><div onclick="dlt(event)">delete</div><span class="date"></span></div>`;
@@ -32,7 +24,7 @@ if (localStorage.money) {
 } else {
   console.log("hi")
   deleteall.style.display = "none"
-  // if (location.hostname !== "mohssineoussama.github.io") { document.documentElement.remove() }
+  if (location.hostname !== "mohssineoussama.github.io") { document.documentElement.remove() }
   money = new Array();
   titels = new Array();
   arrdate = new Array();
